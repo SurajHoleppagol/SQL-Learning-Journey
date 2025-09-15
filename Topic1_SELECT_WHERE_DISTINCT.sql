51c5d3acc9bed017a06232032af8f7288cf6c38e
Suraj Holeppagol <surajholeppagol141@gmail.com>
2025-09-15 17:55:40 +0530

    Topic 1:SELECT,WHERE,DISTINCT

---
+++ Topic1_SELECT_WHERE_DISTINCT.sql
@@ -0,0 +1,33 @@
+/* Topic 1: SELECT, WHERE, DISTINCT */
+
+-- Q1: Show employee names and salaries.
+
+	SELECT ename,sal
+	FROM emp;
+	
+-- Q2: Show all departments.
+
+	SELECT dname
+	FROM dept;
+
+-- Q3: Employees with salary > 2000.
+
+	SELECT ename,sal
+	FROM emp
+	WHERE sal>2000;
+
+-- Q4: Employees hired after 01-Jan-81.
+
+	SELECT ename,hiredate
+	FROM emp
+	WHERE hiredate > '01-Jan-81';
+
+-- Q5: List unique job titles.
+
+	SELECT DISTINCT(job) AS job_titles
+	FROM emp;
+	
+-- Q6: List unique dept numbers.
+
+	SELECT DISTINCT(deptno) AS dept_number
+	FROM emp;
